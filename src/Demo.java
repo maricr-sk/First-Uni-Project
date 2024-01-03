391339351c85a7c2308b37d2564229fe4be02402
maricr-sk <85328841+maricr-sk@users.noreply.github.com>
2024-01-03 18:51:22 -0500

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args) {
+        int celsius = 100;
+        int fahrenheit = 50;
+
+        System.out.println(celsius + "celsius" + "converts to" + fahrenheit + "fahrenheit");
+    }
+}
